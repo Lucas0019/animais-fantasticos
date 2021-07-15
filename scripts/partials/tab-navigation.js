85755179929421a6331373e7ee2a025ae38e31ed
lucas <lxavier.00@outlook.com>
2021-07-15 17:41:44 -0300

    feat: Added tab navigation and add new classes for handling javascriptp

---
+++ scripts/partials/tab-navigation.js
@@ -0,0 +1,22 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if (tabMenu.length && tabContent.length) {
+  //O primeito conteudo sempre com a classe de active tab
+  tabContent[0].classList.add('active');
+  
+  // Adicioanr e Remover a classe Active
+  function activeTab(index) {
+    tabContent.forEach((section) => ( 
+      section.classList.remove('active')
+    ));
+    tabContent[index].classList.add('active');
+  }
+  
+  // Adicionar o evento click no menu de abas
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+    });
+  });
+}
