af7e99bf812a2fb666057ffe1f9b3db914438995
lucas <lxavier.00@outlook.com>
2021-07-15 19:08:26 -0300

    refactor: encapsulated the logic of enabling tab navigation

--- scripts/partials/tab-navigation.js
+++ scripts/partials/tab-navigation.js
@@ -1,22 +1,24 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  //O primeito conteudo sempre com a classe de active tab
-  tabContent[0].classList.add('active');
-  
-  // Adicioanr e Remover a classe Active
-  function activeTab(index) {
-    tabContent.forEach((section) => ( 
-      section.classList.remove('active')
-    ));
-    tabContent[index].classList.add('active');
-  }
-  
-  // Adicionar o evento click no menu de abas
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
+  if (tabMenu.length && tabContent.length) {
+    //O primeito conteudo sempre com a classe de active tab
+    tabContent[0].classList.add("active");
+
+    // Adicioanr e Remover a classe Active
+    function activeTab(index) {
+      tabContent.forEach((section) => section.classList.remove("active"));
+      tabContent[index].classList.add("active");
+    }
+
+    // Adicionar o evento click no menu de abas
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-  });
+  }
 }
+
+initTabNav()
